0480f4f69e56be4788fa8e412455a85eca750eb0
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-11-03 17:23:53 +0100

    Extended OverviewPage.js

--- frontend/src/pages/OverviewPage.js
+++ frontend/src/pages/OverviewPage.js
@@ -1,33 +1,64 @@
 import React from "react";
-import { DataGrid } from '@mui/x-data-grid';
+import {DataGrid} from '@mui/x-data-grid';
+import useCapstones from "../useCapstones";
+import {Typography} from "@mui/material";
+import styled from "styled-components";
 
 export default function OverviewPage() {
-    const rows = [
-        { id: 1, studentName: 'Hello', capstoneLink: 'https://google.com/' , lastCommitOn: '2021-11-01T12:32:16Z'},
-        { id: 2, studentName: 'DataGridPro', capstoneLink: 'https://github.com/' , lastCommitOn: '2021-11-01T12:32:16Z'},
-        { id: 3, studentName: 'MUI', capstoneLink: 'https://google.com/' , lastCommitOn: '2021-11-01T12:32:16Z'},
-    ];
+    const {capstones} = useCapstones()
 
     const renderCapstoneRepoCell = (props) => {
-        console.log(props)
         return (
-            <p onClick={(event) => {
+            <RepoLink onClick={(event) => {
+                window.open(event.target.textContent, '_blank')
+            }}>{props.formattedValue}</RepoLink>
+        )
+    }
+
+    const renderBadge = (props) => {
+        return (
+            <img alt={props.formattedValue} src={props.formattedValue} onClick={(event) => {
                 console.log(event)
-                window.open(event.target.textContent, '_blank')}}>{props.formattedValue}</p>
+                window.open(event.target.src, '_blank')
+            }}/>
+        )
+    }
+
+    const renderCommits = (props) => {
+        return (
+            <Typography>
+                {props.row.mainCommits}({props.row.allCommits})
+            </Typography>
+        )
+    }
+
+    const renderPulls = (props) => {
+        return (
+            <Typography>
+                {props.row.openPulls}({props.row.allPulls})
+            </Typography>
         )
     }
 
     const columns = [
-        { field: 'studentName', headerName: 'Student', width: 150 },
-        { field: 'capstoneLink', headerName: 'Capstone', width: 150 , renderCell: renderCapstoneRepoCell },
-        { field: 'lastCommitOn', headerName: 'Last Commit', width: 170 },
+        {field: 'studentName', headerName: 'Student', width: 150},
+        {field: 'url', headerName: 'Capstone', width: 150, renderCell: renderCapstoneRepoCell},
+        {field: 'updatedAt', headerName: 'Last Commit', width: 170},
+        {field: 'commitCount', headerName: 'Commits', width: 140, renderCell: renderCommits},
+        {field: 'pullsCount', headerName: "PR's", width: 140, renderCell: renderPulls},
+        {field: 'coverageBadgeUrl', headerName: 'Coverage', width: 150, renderCell: renderBadge},
+        {field: 'qualityBadgeUrl', headerName: 'Quality Gate', width: 150, renderCell: renderBadge},
     ];
 
 
 
     return (
-        <div style={{ height: 300, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} />
+        <div style={{height: 800, width: '100%'}}>
+            <DataGrid rows={capstones} columns={columns}/>
         </div>
     )
 }
+
+const RepoLink = styled(Typography)`
+  text-decoration: blue underline;
+`
